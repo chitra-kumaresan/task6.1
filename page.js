035fc104ee5f72ceb20146211a6e07fe9a3473e5
chitra-kumaresan <chitrap803@gmail.com>
2024-01-27 22:00:19 +0530

    my task

---
+++ page.js
@@ -0,0 +1,25 @@
+[{
+    "id": "1",
+    "name": "Lura Senger",
+    "email": "Xander_Collier@yahoo.com"
+  },
+  {
+    "id": "2",
+    "name": "Wilburn Weber",
+    "email": "Bennett_Kreiger11@yahoo.com"
+  },
+  {
+    "id": "3",
+    "name": "Tyrique Hahn",
+    "email": "Juston.Altenwerth@yahoo.com"
+  },
+  {
+    "id": "4",
+    "name": "Lenny Bailey",
+    "email": "Guiseppe_Hegmann@yahoo.com"
+  },
+  {
+    "id": "5",
+    "name": "Vladimir Keeling",
+    "email": "Louisa_Walsh18@hotmail.com"
+  }]
